cead355699bebf76dbcb51dfc804d9bb478bc0fe
Vimal Ranchhod <ranchhod.vimal@gmail.com>
2017-08-12 09:01:43 +0200

    App is working but no Core data

---
+++ CreateTaskViewController.swift
@@ -0,0 +1,55 @@
+//
+//  CreateTaskViewController.swift
+//  Doit
+//
+//  Created by Vimal Ranchhod on 2017/08/10.
+//  Copyright © 2017 Vimal Ranchhod. All rights reserved.
+//
+
+import UIKit
+
+class CreateTaskViewController: UIViewController {
+
+    @IBOutlet weak var importantSwitch: UISwitch!
+    @IBOutlet weak var taskNameTextField: UITextField!
+    
+    var previousVC = TasksViewController()
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func addTapped(_ sender: Any) {
+        //create task from outlet info
+        let task = Task()
+        task.name = taskNameTextField.text!
+        task.important = importantSwitch.isOn
+        
+        //add new task to array in previous view controller
+        previousVC.tasks.append(task)
+        previousVC.tableView.reloadData()
+        navigationController!.popViewController(animated: true)
+      
+        
+        
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
